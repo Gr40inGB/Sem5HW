66bab34228359ac6f3092cbff40639c5bc0a2330
Rishat_Imangulov <gr40in@yandex.ru>
2023-01-30 01:27:20 +0500

    task1-34 done

--- Task1(34)/Program.cs
+++ Task1(34)/Program.cs
@@ -3,3 +3,36 @@
 
 // [345, 897, 568, 234] -> 2
 
+int[] GetFilledArray(int size, int minValue, int maxValue)
+{
+    int[] ArrayForFill = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        ArrayForFill[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return ArrayForFill;
+}
+
+int CheckByTwo(int[] ArrayForCheck)
+{
+    int countByTwo = 0;
+    for (int j = 0; j < ArrayForCheck.Length; j++)
+    {
+        if (ArrayForCheck[j] % 2 == 0)
+        {
+            countByTwo++;
+        }
+    }
+    return countByTwo;
+}
+
+void main()
+{
+    int randomSize = new Random().Next(6, 30);
+    int[] OurArray = GetFilledArray(randomSize, 100, 999);
+    System.Console.WriteLine($"Сгенерирован массив из {randomSize} значений:\n{String.Join(" ", OurArray)}");
+    System.Console.WriteLine($" Чётных значений в массиве {CheckByTwo(OurArray)} шт");
+}
+
+main();
+
