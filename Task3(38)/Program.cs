ba0229ee6bc32cb7ee101e9287796e9c35e2e0e4
Rishat_Imangulov <gr40in@yandex.ru>
2023-01-30 13:09:07 +0500

    task3-38 done

--- Task3(38)/Program.cs
+++ Task3(38)/Program.cs
@@ -3,7 +3,6 @@
 
 // [3 7 22 2 78] -> 76
 
-
 double[] GetFilledArrayDouble(int size)
 {
     double[] ArrayForFill = new double[size];
@@ -17,33 +16,55 @@ double[] GetFilledArrayDouble(int size)
     return ArrayForFill;
 }
 
-int[] SummInNotByTwoIndex(int[] ArrayForCheck)
+int[] FindMinMax(double[] ArrayForCheck)
 {
-    int countIndex = 0;
-    int summ = 0;
-    for (int j = 1; j < ArrayForCheck.Length; j += 2)
+    int minIndex = 0;
+    int maxIndex = 0;
+    for (int j = 0; j < ArrayForCheck.Length; j++)
     {
-        countIndex++;
-        summ += ArrayForCheck[j];
+        minIndex = (ArrayForCheck[minIndex] < ArrayForCheck[j]) ? minIndex : j;
+        maxIndex = (ArrayForCheck[maxIndex] > ArrayForCheck[j]) ? maxIndex : j;
     }
-    return new[] { countIndex, summ };
+    return new[] { minIndex, maxIndex };
 }
 
 void main()
 {
     Console.Clear();
-    int randomSize = new Random().Next(6, 30);
+    int randomSize = new Random().Next(6, 12);
     double[] OurArray = GetFilledArrayDouble(randomSize);
-    System.Console.WriteLine($"Сгенерирован массив из {randomSize} значений:\n");
+    System.Console.WriteLine($"Сгенерирован массив из {randomSize} значений:");
 
+    int[] MinMaxArray = FindMinMax(OurArray);
+    int minOurArray = MinMaxArray[0];
+    int maxOurArray = MinMaxArray[1];
+
+    int j = 0; // переменная для оформления
     for (int i = 0; i < randomSize; i++)
     {
-        System.Console.Write(" {0,20:F8} ", OurArray[i]);
-        System.Console.WriteLine();
-    }
+        if (i == minOurArray)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+        }
+        else if (i == maxOurArray)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+        }
 
-    // int[] ResultArray = SummInNotByTwoIndex(OurArray);
-    // System.Console.WriteLine($" Сумма значений на нечётных ({ResultArray[0]} шт) позициях равна {ResultArray[1]} ");
+        System.Console.Write(" {0,15:F4} ", OurArray[i]);
+        j++;
+        if (j % 5 == 0) System.Console.WriteLine(); // на каждой строке максимум 5 значений
+    }
+    System.Console.WriteLine();
+    Console.ForegroundColor = ConsoleColor.White;
+    System.Console.Write("Разница между максимальным и минимальным значениями массива равна: ");
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+    System.Console.WriteLine(" {0:F4} ", OurArray[maxOurArray] - OurArray[minOurArray]);
+    Console.ForegroundColor = ConsoleColor.White;
 }
 
 main();
